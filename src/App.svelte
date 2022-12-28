a34e928578951f63e37e8a979671449ac2299054
Zen <muhzaini30@gmail.com>
2022-12-28 22:23:30 +0800

    Oke

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,14 @@
 <script>
   import Router from "svelte-spa-router";
   import routes from "virtual:generated-pages-svelte";
-
-  function tutup() {
-    document.querySelector(".hamburger").click();
-  }
+  import Header from "header-apk";
 </script>
 
-<div data-role="appbar" data-expand-point="md">
-  <span class="brand no-hover"> Kaligrafi </span>
-
-  <ul class="app-bar-menu">
-    <li><a on:click={tutup} href="#/">Home</a></li>
-    <li><a on:click={tutup} href="#/donasi">Support Me</a></li>
-    <li>
-      <a
-        on:click={tutup}
-        href="https://play.google.com/store/apps/details?id=com.trihandayani.kaligrafi"
-        >Give 5 Stars</a
-      >
-    </li>
-  </ul>
-</div>
+<Header
+  judul="Kaligrafi"
+  id="com.trihandayani.kaligrafi"
+  afiliasi="https://tokopedia.link/Sji7LMb17vb"
+/>
 <div class="isi">
   <Router {routes} />
 </div>
@@ -32,9 +19,6 @@
   }
   :global(*) {
     word-wrap: break-word;
-  }
-  .wadah {
-    min-height: 100vh;
   }
   /* chrome extension */
   /* :global(body) {
